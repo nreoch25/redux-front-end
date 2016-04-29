a1848214e45664f22afe60650405426a106caa8f
Nigel Reoch <nigel.reoch@cbc.ca>
2016-04-28 20:46:30 -0400

    Added redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+
+
+export function signinUser({ email, password }) {
+  return (dispatch) => {
+    //Submit email/password to the server
+
+    //If Request is good...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - Redirect to the route "/feature"
+
+    //If Request is bad...
+    // - Show an error to the user
+  }
+}
