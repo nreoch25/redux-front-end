66a816edc4d4d23b229b5fdf7b8440a26e1917b3
Nigel Reoch <nigel.reoch@cbc.ca>
2016-05-01 14:28:24 -0400

    Added auth reducer and localStore for token

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,24 @@
 import axios from "axios";
+import { browserHistory } from "react-router";
+import { AUTH_USER } from "./types";
 const API_URL = "http://localhost:3090";
 
 export function signinUser({ email, password }) {
   return (dispatch) => {
     //Submit email/password to the server
-    axios.post(`${API_URL}/signin`, { email, password });
-
-    //If Request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - Redirect to the route "/feature"
-
-    //If Request is bad...
-    // - Show an error to the user
+    axios.post(`${API_URL}/signin`, { email, password })
+      .then((response) => {
+        //If Request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        localStorage.setItem("token", response.data.token);
+        // - Redirect to the route "/feature"
+        browserHistory.push("/feature");
+      })
+      .catch(() => {
+        //If Request is bad...
+        // - Show an error to the user
+      });
   }
 }
