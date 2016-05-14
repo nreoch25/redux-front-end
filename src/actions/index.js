7f31bf455f0a30cec410a34c83c29fcdb6869ac6
Nigel Reoch <nigel.reoch@cbc.ca>
2016-05-14 12:01:29 -0400

    client side jwt

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { browserHistory } from "react-router";
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from "./types";
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from "./types";
 const API_URL = "http://localhost:8000";
 
 export function signinUser({ email, password }) {
@@ -47,8 +47,21 @@ export function authError(error) {
 
 export function signoutUser() {
   console.log("signout");
-  //localStorage.removeItem("token");
+  localStorage.removeItem("token");
   return {
     type: UNAUTH_USER
   };
+}
+export function fetchMessage() {
+  return function(dispatch) {
+    axios.get(API_URL, {
+      headers: { authorization: localStorage.getItem("token") }
+    })
+      .then(response => {
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
+        });
+      });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -6,19 +6,30 @@ import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import reduxThunk from "redux-thunk";
 
 import App from './components/app';
+import Index from "./components/index";
 import Signin from "./components/auth/signin";
 import Signout from "./components/auth/signout";
 import Signup from "./components/auth/signup";
 import Feature from "./components/feature";
 import RequireAuth from "./components/auth/require_auth";
 import reducers from './reducers';
+import { AUTH_USER } from "./actions/types";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem("token");
+
+// If we have a token, consider the user to be signed in
+if(token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Index} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
